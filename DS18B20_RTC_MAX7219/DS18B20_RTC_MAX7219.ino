1449deb7e883bcf534d161bed55f8a6b301402fc
costarodriguesagap <36197226+costarodriguesagap@users.noreply.github.com>
2019-04-18 12:03:05 +0100

    Update DS18B20_RTC_MAX7219.ino

--- DS18B20_RTC_MAX7219/DS18B20_RTC_MAX7219.ino
+++ DS18B20_RTC_MAX7219/DS18B20_RTC_MAX7219.ino
@@ -1,18 +1,3 @@
-// Use the Parola library to scroll text on the display
-//
-// Demonstrates the use of the scrolling function to display text received
-// from the serial interface
-//
-// User can enter text on the serial monitor and this will display as a
-// scrolling message on the display.
-// Speed for the display is controlled by a pot on SPEED_IN analog in.
-// Scrolling direction is controlled by a switch on DIRECTION_SET digital in.
-// Invert ON/OFF is set by a switch on INVERT_SET digital in.
-//
-// UISwitch library can be found at https://github.com/MajicDesigns/MD_UISwitch
-// MD_MAX72XX library can be found at https://github.com/MajicDesigns/MD_MAX72XX
-//
-
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
@@ -71,8 +56,6 @@ void setup()
   
   P.begin();
   for (uint8_t i=0 ; i<MAX_DEVICES ; i++){P.setIntensity(i,0);}
-//  P.setFont(BigFont);
-  //P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffectIn, scrollEffectOut);
   sensors.begin();
   if(!sensors.getAddress(sensor1, 0))
   {
@@ -99,15 +82,10 @@ void loop()
       strcpy(curMessage, "Temp: ");
       dtostrf(temp_Act , 2, 2, newMessage);
       strcat( curMessage, newMessage );strcat( curMessage, String("\xF7").c_str() );
-      
-//      if (temp_Act < temp_Ant){scrollEffectIn = PA_SCROLL_DOWN;} // Temperatura Baixou
-//      else{scrollEffectIn = PA_SCROLL_UP;} // Temperatura Subiu 
-//      if (temp_Act < temp_Ant){scrollEffectIn = PA_SLICE;} // Temperatura Baixou
-//      else{scrollEffectIn = PA_SLICE;} // Temperatura Subiu
-//      if (temp_Act < temp_Ant){scrollEffectIn = PA_SCAN_HORIZ;} // Temperatura Baixou
-//      else{scrollEffectIn = PA_SCAN_HORIZ;} // Temperatura Subiu
+
       if (temp_Act < temp_Ant){scrollEffectIn = PA_SCROLL_DOWN_LEFT;} // Temperatura Baixou
       else{scrollEffectIn = PA_SCROLL_UP_LEFT;} // Temperatura Subiu
+      
       scrollPause = 0;scrollSpeed = 30;
       P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffectIn);P.displayReset();
       temp_Ant=temp_Act;
